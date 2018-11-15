8eb89c7d899e4719547dba7324a2e20fc19871ee
armaganf <ferdi.armagan@stud.hs-hannover.de>
2018-11-15 11:43:17 +0100

    Changed inputs, fixed redirection after beat creation, added editing/removing of beats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ferdi = User.create(email: 'ferdiarmagan@web.de', password: 'ferdiamg', password_confirmation: 'ferdiamg')
-maik = User.create(email: 'maikreinicke@yahoo.de', password: 'maik123', password_confirmation: 'maik123')
+User.create(email: 'ferdiarmagan@web.de', password: 'ferdiamg', password_confirmation: 'ferdiamg')
+User.create(email: 'maikreinicke@yahoo.de', password: 'maik123', password_confirmation: 'maik123')
 
-ferdi.beats.new(name: 'Astro', bpm: '130', key: 'F#m', link: 'https://www.dropbox.com/s/8lnnk2ts0wimhnw/Cartii%20Yachty%20C%20Minor%20150BPM%20v2.mp3')
-ferdi.beats.new(name: 'Fuego', bpm: '144', key: 'D#m', link: 'https://www.dropbox.com/s/8lnnk2ts0wimhnw/Cartii%20Yachty%20C%20Minor%20150BPM%20v2.mp3')
-ferdi.beats.new(name: 'Aliens', bpm: '150', key: 'F#', link: 'https://www.dropbox.com/s/huo1u4d57ubl44k/G%23%20Minor%20PartyNextDoor%20Type%20Beat%20Wus%20Good.mp3')
+Beat.create(name: 'Astro', bpm: '130', key: 'F#m', link: 'https://www.dropbox.com/s/8lnnk2ts0wimhnw/Cartii%20Yachty%20C%20Minor%20150BPM%20v2.mp3', user_id: 1)
+Beat.create(name: 'Fuego', bpm: '144', key: 'D#m', link: 'https://www.dropbox.com/s/8lnnk2ts0wimhnw/Cartii%20Yachty%20C%20Minor%20150BPM%20v2.mp3', user_id: 1)
+Beat.create(name: 'Aliens', bpm: '150', key: 'F#', link: 'https://www.dropbox.com/s/huo1u4d57ubl44k/G%23%20Minor%20PartyNextDoor%20Type%20Beat%20Wus%20Good.mp3', user_id: 2)
