b98142d9e83b85ae73806b8171b2309c3f8b001d
armaganf <ferdi.armagan@stud.hs-hannover.de>
2018-11-15 14:39:31 +0100

    Added usernames to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :username, :password, :password_confirmation)
     end
 end
