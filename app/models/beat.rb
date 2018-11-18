6a13dcab566fef31070dd9b98103bc7ce0f832b4
armaganf <ferdi.armagan@stud.hs-hannover.de>
2018-11-18 11:59:01 +0100

    Added tagging to the beat creation

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,9 +1,12 @@
 class Beat < ApplicationRecord
   validates :name, length: { minimum: 3, maximum: 15 }
-  validates :bpm, length: { maximum: 3 }
+  validates :bpm, format: { with: /\A\d+\z/ }, length: { maximum: 3 }
   validates :key, length: { maximum: 3 }
   validates :link, presence: true
 
   has_many :artists
   belongs_to :user
+
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable_on :artists
 end
