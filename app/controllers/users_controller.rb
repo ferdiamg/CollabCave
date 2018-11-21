37daaf6f065312bee5b3ec46688055a75cd4e5a7
armaganf <ferdi.armagan@stud.hs-hannover.de>
2018-11-21 22:12:35 +0100

    Minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    redirect_to :root unless @user.id == current_user.id
+  end
+
   def update
+    @user = User.find(params[:id])
+    redirect_to :root unless @user.id == current_user.id
     if @user.update(user_params)
+      redirect_to edit_user_path, notice: 'Successfully updated your profile.'
     else
+      redirect_to edit_user_path, notice: "Couldn't update your profile."
     end
   end
 
   def destroy
-    @user.destroy
+    @user = User.find(params[:id])
+    redirect_to :root unless @user.id == current_user.id
+    if @user.destroy
+      redirect_to :root, notice: 'Thats sad, see you soon!'
+    else 
+      redirect_to edit_user_path, notice: "Account couldn't be deleted"
+    end
   end
 
   private
