6ed6707383449fc20c8d53fe9fd9c0634403f323
armaganf <ferdi.armagan@stud.hs-hannover.de>
2018-12-07 01:35:12 +0100

    Fixed AJAX like functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,23 +2,28 @@ class LikesController < ApplicationController
   
   def create
     @beat = Beat.find(params[:beat_id])
-
-    if already_liked?
-      flash.now[:notice] = "You can't like more than once!"
-    else
-      @beat.likes.create(user_id: current_user.id)
-      redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      if already_liked?
+        flash.now[:notice] = "You can't like more than once!"
+      else
+        @beat.likes.create(user_id: current_user.id)
+        # redirect_back(fallback_location: root_path)
+        format.js { render 'beats/create.js.erb'}
+      end
     end
   end
 
   def destroy
     @beat = Beat.find(params[:beat_id])
     @like = @beat.likes.find(params[:id])
-    if !already_liked?
-      flash.now[:notice] = "Can't unlike!"
-    else
-      @like.destroy
-      redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      if !already_liked?
+        flash.now[:notice] = "Can't unlike!"
+      else
+        @like.destroy
+        format.js { render 'beats/create.js.erb'}
+        # redirect_back(fallback_location: root_path)
+      end
     end
   end
 
