f79facd0c20494f04f34ac0d93788dee22fc502b
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-01-29 23:10:35 +0100

    Changed forgot password page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,18 +59,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: "smtp.gmail.com",
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: "info.collabcave",
-  #   password: "ENV['EMAILPAROLE']"
-  # }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "smtp.gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "info.collabcave",
+    password: "!collabcave1995"
+  }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   resources :user_sessions, :except => [:index]
   resources :users
+  resources :password_resets
   resources :collaboration_requests, only: [:create, :destroy, :update]
   resources :beats do
     resources :likes, :stems, only: [:create, :destroy]
   end
+  
 
   get 'login' => 'user_sessions#new', :as => :login
   get 'register' => 'users#new', :as => :register
