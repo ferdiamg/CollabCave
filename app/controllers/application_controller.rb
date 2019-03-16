f183c2a8231685f62be8facf91c5fe94a0079290
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-03-16 21:46:01 +0100

    Fixed redirecting issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
-  def index
-  end
-
   private
     def not_authenticated
       redirect_to login_path, alert: 'Please login first!'
