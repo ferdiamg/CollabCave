a9e1e065d164eeebd6ff929b76bedb414f55d9e4
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-03-16 21:46:01 +0100

    Fixed redirecting issues

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,5 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, except: [:destroy]
   before_action :disable_header, only: [:new]
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  get 'password_resets/create'
-  get 'password_resets/edit'
-  get 'password_resets/update'
   resources :user_sessions, :except => [:index]
   resources :users
-  resources :password_resets
-  resources :collaboration_requests, only: [:create, :destroy, :update]
   resources :beats do
     resources :likes, :stems, only: [:create, :destroy]
   end
-  
 
   get 'login' => 'user_sessions#new', :as => :login
   get 'register' => 'users#new', :as => :register
-  get 'collaborations' => 'collaboration_requests#index', :as => :collaborations
-  get 'feed' => 'beats#index', :as => :feed
   post 'logout' => 'user_sessions#destroy', :as => :logout
   root 'user_sessions#new'
+
+  resources :collaboration_requests, only: [:create, :destroy, :update]
+  resources :password_resets
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
+  get 'collaborations' => 'collaboration_requests#index', :as => :collaborations
+  get 'feed' => 'beats#index', :as => :feed
 end
